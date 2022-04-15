4d5d73343817189338f96a5b832bca206fa5a49c
Pavel <impetus@yandex.ru>
2022-04-15 13:07:22 +0300

    change comment

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 
-    // comment
+    // comment 1234567
 
     @Autowired
     PizzaRepository repository;
